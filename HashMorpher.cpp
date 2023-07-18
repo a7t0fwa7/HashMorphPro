d4bccc15b5cc82f00db9de8301a58889001dfd2e
a7t0fwa7 <43460290+a7t0fwa7@users.noreply.github.com>
2023-07-18 22:27:49 +0800

    Update HashMorphPro.cpp

--- HashMorpher.cpp
+++ HashMorpher.cpp
@@ -3,6 +3,7 @@
 #include <stdbool.h>
 #include <string.h>
 #include <wincrypt.h>
+#include <time.h>
 
 #define ANSI_COLOR_RED     "\x1b[31m"
 #define ANSI_COLOR_GREEN   "\x1b[32m"
@@ -11,118 +12,85 @@
 #define ANSI_COLOR_BLUE    "\x1b[34m"
 #define ANSI_COLOR_RESET   "\x1b[0m"
 
-bool copyFile(const char* sourcePath, const char* destinationPath) {
-    if (!CopyFileA(sourcePath, destinationPath, FALSE)) {
-        printf(ANSI_COLOR_RED);
-        printf("Failed to copy file. Error code: %d\n", GetLastError());
-        printf(ANSI_COLOR_RESET);
+bool morphFile(const char* sourcePath, const char* destinationPath) {
+    FILE* source = fopen(sourcePath, "rb");
+    FILE* destination = fopen(destinationPath, "wb");
+    if (!source || !destination) {
+        return false;
+    }
+    size_t bytesRead;
+    char buffer[4096];
+    while ((bytesRead = fread(buffer, 1, sizeof(buffer), source)) > 0) {
+        fwrite(buffer, 1, bytesRead, destination);
+    }
+    fclose(source);
+    fclose(destination);
+
+    destination = fopen(destinationPath, "r+b");
+    if (!destination) {
         return false;
     }
 
-    HANDLE destinationFile = CreateFileA(destinationPath, FILE_APPEND_DATA, FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
-    if (destinationFile == INVALID_HANDLE_VALUE) {
-        printf(ANSI_COLOR_RED);
-        printf("Failed to open destination file. Error code: %d\n", GetLastError());
-        printf(ANSI_COLOR_RESET);
-        return false;
+    fseek(destination, 0, SEEK_END);
+    long fileSize = ftell(destination);
+
+    int randomBytes = rand() % 1024;
+    int randomPosition = rand() % fileSize;
+
+    fseek(destination, randomPosition, SEEK_SET);
+    char randomData[randomBytes];
+    for (int i = 0; i < randomBytes; i++) {
+        randomData[i] = rand() % 256;
     }
+    fwrite(randomData, 1, randomBytes, destination);
 
-    srand(GetTickCount());  // Initialize the random seed based on the system tick count
-    unsigned char byte1 = rand() % 256;  // Generate a random value between 0 and 255
-    unsigned char byte2 = rand() % 256;
-
-    DWORD bytesWritten;
-    if (!WriteFile(destinationFile, &byte1, sizeof(byte1), &bytesWritten, NULL) ||
-        !WriteFile(destinationFile, &byte2, sizeof(byte2), &bytesWritten, NULL)) {
-        printf(ANSI_COLOR_RED);
-        printf("Failed to write bytes to file. Error code: %d\n", GetLastError());
-        printf(ANSI_COLOR_RESET);
-        CloseHandle(destinationFile);
-        return false;
-    }
-
-    CloseHandle(destinationFile);
+    fclose(destination);
 
     return true;
 }
 
-void printFileHash(const char* filePath) {
-    HANDLE fileHandle = CreateFileA(filePath, GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
-    if (fileHandle == INVALID_HANDLE_VALUE) {
-        printf(ANSI_COLOR_RED);
-        printf("Failed to open file. Error code: %d\n", GetLastError());
-        printf(ANSI_COLOR_RESET);
+void morphFilesInDirectory(const char* directoryPath) {
+    WIN32_FIND_DATA findData;
+    HANDLE hFind = FindFirstFile(directoryPath, &findData);
+    if (hFind == INVALID_HANDLE_VALUE) {
         return;
     }
 
-    HCRYPTPROV hCryptProv;
-    if (!CryptAcquireContext(&hCryptProv, NULL, NULL, PROV_RSA_FULL, CRYPT_VERIFYCONTEXT)) {
-        printf(ANSI_COLOR_RED);
-        printf("Failed to acquire cryptographic context. Error code: %d\n", GetLastError());
-        printf(ANSI_COLOR_RESET);
-        CloseHandle(fileHandle);
-        return;
-    }
-
-    HCRYPTHASH hHash;
-    if (!CryptCreateHash(hCryptProv, CALG_MD5, 0, 0, &hHash)) {
-        printf(ANSI_COLOR_RED);
-        printf("Failed to create hash object. Error code: %d\n", GetLastError());
-        printf(ANSI_COLOR_RESET);
-        CryptReleaseContext(hCryptProv, 0);
-        CloseHandle(fileHandle);
-        return;
-    }
-
-    const int bufferSize = 8192;
-    BYTE buffer[bufferSize];
-    DWORD bytesRead;
-    while (ReadFile(fileHandle, buffer, bufferSize, &bytesRead, NULL) && bytesRead > 0) {
-        if (!CryptHashData(hHash, buffer, bytesRead, 0)) {
-            printf(ANSI_COLOR_RED);
-            printf("Failed to hash data. Error code: %d\n", GetLastError());
-            printf(ANSI_COLOR_RESET);
-            CryptDestroyHash(hHash);
-            CryptReleaseContext(hCryptProv, 0);
-            CloseHandle(fileHandle);
-            return;
+    do {
+        if (findData.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) {
+            continue;
         }
-    }
 
-    BYTE hashValue[16];
-    DWORD hashSize = sizeof(hashValue);
-    if (CryptGetHashParam(hHash, HP_HASHVAL, hashValue, &hashSize, 0)) {
-        printf("MD5: ");
-        for (DWORD i = 0; i < hashSize; i++) {
-            printf("%02x", hashValue[i]);
+        char* extension = strrchr(findData.cFileName, '.');
+        if (extension == NULL) {
+            continue;
         }
-        printf("\n");
-    }
-    else {
-        printf(ANSI_COLOR_RED);
-        printf("Failed to retrieve hash value. Error code: %d\n", GetLastError());
-        printf(ANSI_COLOR_RESET);
-    }
 
-    CryptDestroyHash(hHash);
-    CryptReleaseContext(hCryptProv, 0);
-    CloseHandle(fileHandle);
+        if (strcmp(extension, ".exe") != 0 && strcmp(extension, ".dll") != 0 && strcmp(extension, ".ps1") != 0) {
+            continue;
+        }
+
+        char destinationPath[MAX_PATH];
+        sprintf(destinationPath, "%s\\%s", directoryPath, findData.cFileName);
+        morphFile(findData.cFileName, destinationPath);
+    } while (FindNextFile(hFind, &findData));
+
+    FindClose(hFind);
 }
 
 int main() {
+    srand(time(NULL));
+
     int choice;
     const char* sourcePath;
     char destinationPath[MAX_PATH] = "";
 
-    // ASCII art
     printf(ANSI_COLOR_RED);
-    printf("\t\t\t|_| _  _|_ |\\/| _  _ _ |_  _  _\n");
-    printf("\t\t\t| |(_|_\\| ||  |(_)| |_)| |(/_| \n");
-    printf("\t\t\t                    |           \n");
+    printf("\t\t\tHash Morph Pro\n");
     printf(ANSI_COLOR_RESET);
     printf(ANSI_COLOR_BLUE);
-    printf("\t\t\t\t\tBy Abdulaziz Almetairy\n");
-    printf("\t\t\t\t\t   github.com/oh-az\n\n\n");
+    printf("\t\t\t\t\tBy a7t0fwa7\n");
+    printf("\t\t\t\t\tgithub.com/a7t0fwa7\n\n\n");
     printf(ANSI_COLOR_RESET);
 
     printf(ANSI_COLOR_GREEN);
@@ -159,20 +127,20 @@ int main() {
         printf(ANSI_COLOR_RESET);
         return 1;
     }
+
     printf(ANSI_COLOR_GREEN);
     printf("Enter destination path (leave empty for current directory): ");
     printf(ANSI_COLOR_RESET);
-    getchar();  // Consume the newline character after choice input
+    getchar();
 
     char input[MAX_PATH];
     fgets(input, sizeof(input), stdin);
-    input[strcspn(input, "\n")] = '\0';  // Remove trailing newline character
+    input[strcspn(input, "\n")] = '\0';
 
     if (strcmp(input, "") != 0) {
         strncpy_s(destinationPath, sizeof(destinationPath), input, _TRUNCATE);
     }
     else {
-        // If the destination path is empty, use the current directory with the copied file name appended with "_new"
         const char* fileName = strrchr(sourcePath, '\\');
         if (fileName == NULL) {
             printf(ANSI_COLOR_RED);
@@ -180,7 +148,7 @@ int main() {
             printf(ANSI_COLOR_RESET);
             return 1;
         }
-        fileName++;  // Move past the '\' character
+        fileName++;
         const char* dot = strrchr(fileName, '.');
         if (dot != NULL) {
             strncpy_s(destinationPath, sizeof(destinationPath), fileName, dot - fileName);
@@ -193,7 +161,7 @@ int main() {
         }
     }
 
-    if (copyFile(sourcePath, destinationPath)) {
+    if (morphFile(sourcePath, destinationPath)) {
         char fullPath[MAX_PATH];
         if (GetFullPathNameA(destinationPath, sizeof(fullPath), fullPath, NULL) == 0) {
             printf(ANSI_COLOR_RED);
